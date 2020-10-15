331bc1145296300c4a22380f71d02a295be492f8
Robert Bizon <m4stersplint3r@gmail.com>
2020-10-15 09:06:33 -0400

    Changes

--- Program.cs
+++ Program.cs
@@ -12,7 +12,6 @@ namespace Day3_Lab6_PowersTables
         {
             return x * x * x;
         }
-
         static void Main(string[] args)
         {
             int userInput = 0;
@@ -24,15 +23,17 @@ namespace Day3_Lab6_PowersTables
                 do
                 {
                     Console.Write("Hello, please enter an integer: ");
-                } while (!int.TryParse(Console.ReadLine(), out userInput));
-                
+                } while (!int.TryParse(Console.ReadLine(), out userInput));                
 
-                Console.WriteLine($"{Environment.NewLine}Number\tSquared\tCubed");
-                Console.WriteLine($"======\t======\t=====");
+                //Console.WriteLine($"{Environment.NewLine}Number\tSquared\tCubed");
+                //Console.WriteLine($"======\t======\t=====");
+                Console.WriteLine(Environment.NewLine + "{0,-10} {1,-10} {2,-10}", "Number", "Squared", "Cubed");
+                Console.WriteLine("{0,-10} {1,-10} {2,-10}", "======", "======", "======");
 
                 for (int i = 1; i <= userInput; i++)
                 {
-                    Console.WriteLine($"{i}\t{squareNumber(i)}\t{cubeNumber(i)}");
+                    //Console.WriteLine($"{i}\t{squareNumber(i)}\t{cubeNumber(i)}");
+                    Console.WriteLine("{0,-10} {1,-10} {2,-10}", i, squareNumber(i), cubeNumber(i));
                 }
 
                 Console.Write($"{Environment.NewLine}Would you like to continue? (y/n): ");
